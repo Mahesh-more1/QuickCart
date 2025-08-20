f15f78a525ccec7650bbf5a5d2071dc5eb1138fa
Mahesh-more1 <mmore2743@gmail.com>
2025-08-20 20:51:09 +0530

    update message

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/QuickCart-EcomWebsite/', // Your repo name with trailing slash
+  base: '/QuickCartEcomProject/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),                     // root
+        about: resolve(__dirname, 'src/about.html'),                // src folder
+        contact: resolve(__dirname, 'src/contact.html'),
+        products: resolve(__dirname, 'src/products.html'),
+        addToCart: resolve(__dirname, 'src/add-to-cart.html'),
+        signIn: resolve(__dirname, 'src/sign-in.html'),
+        signUp: resolve(__dirname, 'src/sign-up.html'),             // keep your filename typo if exists
+      },
+    },
+  },
 });
